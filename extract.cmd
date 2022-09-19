36a5d28c203036cf839ca966a75e651aa25ddaa2
Wilderness Ranger <wildernessrgr@aliyun.com>
2022-09-19 19:37:45 +0800

    Create script

---
+++ extract.cmd
@@ -0,0 +1,88 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+:init-param
+set bindir=%~dp0\bin
+set codebook=packed_codebooks_aoTuV_603.bin
+
+set input=.
+set output=.\output
+set tempdir=%temp%\tmp%random%
+
+if "%~1" neq "" (
+ set input=%~1
+)
+if "%~2" neq "" (
+ set output=%~2
+)
+
+:check-env
+echo ===== Genshin Impact Audio Extractor =====
+echo.
+where /q java || (
+ echo Error: Cannot find Java in environment.
+ pause
+ exit /b 2
+)
+
+:check-param
+if not exist "%input%" (
+ echo Error: Input does not exist.
+ pause
+ exit /b 1
+)
+if not exist "%output%" (
+ md "%output%" || (
+  pause
+  exit /b 2
+ )
+) else if not exist "%output%\*" (
+ echo Error: Output is not a directory.
+ pause
+ exit /b 1
+)
+md "%tempdir%" || (
+ echo Error: Cannot create temp directory.
+ pause
+ exit /b 2
+)
+
+:process
+"%bindir%\quickbms.exe" -F {}.pck "%bindir%\wavescan.bms" "%input%" "%tempdir%" || (
+ rd /s /q "%tempdir%"
+ pause
+ exit /b 2
+)
+echo.
+for %%f in ("%tempdir%\*.wav") do (
+ echo Processing "%%~nxf"...
+ java -jar "%bindir%\ptadpcminflate.jar" "%%f" "%output%\%%~nf.wav"
+ if !errorlevel! equ 0 (
+  del "%%f"
+  echo.
+ ) else if !errorlevel! equ 1 (
+  echo.
+  "%bindir%\ww2ogg\ww2ogg.exe" "%%f" -o "%%~dpnf.ogg" --pcb "%bindir%\ww2ogg\%codebook%" && (
+   "%bindir%\revorb.exe" "%%~dpnf.ogg" "%output%\%%~nf.ogg" && (
+    del "%%f" "%%~dpnf.ogg"
+   )
+  ) || echo Warning: Failed to convert "%%f" to OGG.
+  echo.
+ ) else (
+  del "%output%\%%~nf.wav" 2>nul
+  echo Warning: Failed to parse "%%f".
+  echo.
+ )
+)
+
+:cleanup
+if exist "%tempdir%\*.wav" (
+ if not exist "%output%\bad" (
+  md "%output%\bad" 2>nul
+ )
+ move /y "%tempdir%\*.wav" "%output%\bad"
+ echo Failed to process some files. Moved to "%output%\bad".
+)
+rd /s /q "%tempdir%"
+echo Extraction completed.
+pause
